71df34eb5bd9b0fa421c376ca811e5f8070a18c5
Harshit671 <harshitgpt45@gmail.com>
2023-08-20 12:26:35 +0530

    hii

--- src/Components/SecondPage.jsx
+++ src/Components/SecondPage.jsx
@@ -6,6 +6,7 @@ import "./bootstrap.css"
 
 function SecondPage() {
   const [sliderValue, setSliderValue] = useState(50); // Initial slider value
+  const [createBattles,setCreateBattles]=useState();
 
   const handleSliderChange = (event) => {
     const newValue = parseInt(event.target.value);
@@ -37,12 +38,28 @@ function SecondPage() {
       prize: 50,
     },
     {
-      players: ['Avni', 'Opponent'],
-      playingFor: 50,
-      prize: 50,
+      players: ['John', 'Jane'],
+      playingFor: 30,
+      prize: 30,
     },
-    // Add more running battles as needed
+    {
+      players: ['Alice', 'Bob'],
+      playingFor: 25,
+      prize: 25,
+    },
+    
+    {
+      players: ['User1', 'User2'],
+      playingFor: 40,
+      prize: 40,
+    },
+    {
+      players: ['PlayerA', 'PlayerB'],
+      playingFor: 60,
+      prize: 60,
+    },
   ];
+
 
   const navigate = useNavigate();
 
@@ -83,34 +100,37 @@ function SecondPage() {
                     <h6 className="mb-0">Rules <i className="bi bi-info-circle" /></h6>
                   </div>
                 </div>
-                {openBattles.map((battle, index) => (
-                  <div className="col-12 card my-1 walletcard pt-2 px-0 mx-auto text-white" key={index}>
-                    <div className="row">
-                      <div className="col">
-                        <h6 className="mx-2">CHALLENGE FROM <span className="text-danger">{battle.challenger}</span></h6>
-                      </div>
-                    </div>
-                    <div className="card-body walletbody mt-2">
+                <div className="scroll-container">
+                  {openBattles.map((battle, index) => (
+                    <div className="col-12 card my-1 walletcard pt-2 px-0 mx-auto text-white" key={index}>
                       <div className="row">
-                        <div className="col-12 d-flex justify-content-between">
-                          <div className="d-flex">
-                            <div className="me-2">
-                              <p className="mb-0" id="win-time">Entry Fee</p>
-                              <h6 className="mb-0 d-flex"><span className="material-symbols-outlined text-success">payments</span> {battle.entryFee}</h6>
+                        <div className="col">
+                          <h6 className="mx-2">CHALLENGE FROM <span className="text-danger">{battle.challenger}</span></h6>
+                        </div>
+                      </div>
+                      <div className="card-body walletbody mt-2">
+                        <div className="row">
+                          <div className="col-12 d-flex justify-content-between">
+                            <div className="d-flex">
+                              <div className="me-2">
+                                <p className="mb-0" id="win-time">Entry Fee</p>
+                                <h6 className="mb-0 d-flex"><span className="material-symbols-outlined text-success">payments</span> {battle.entryFee}</h6>
+                              </div>
+                              <div className="ms-2">
+                                <p className="mb-0" id="win-time">Prize</p>
+                                <h6 className="mb-0 d-flex"><span className="material-symbols-outlined text-success">payments</span> {battle.prize}</h6>
+                              </div>
                             </div>
-                            <div className="ms-2">
-                              <p className="mb-0" id="win-time">Prize</p>
-                              <h6 className="mb-0 d-flex"><span className="material-symbols-outlined text-success">payments</span> {battle.prize}</h6>
+                            <div>
+                              <button className="btn bg-orange">Play</button>
                             </div>
-                          </div>
-                          <div>
-                            <button className="btn bg-orange">Play</button>
                           </div>
                         </div>
                       </div>
                     </div>
-                  </div>
-                ))}
+                  ))}
+                </div>
+               
               </div>
             </div>
             <div className="col-12 my-2 bg-purple2 py-3">
@@ -120,32 +140,35 @@ function SecondPage() {
                     <h6 className="mb-0"><i className="bi bi-x-circle-fill text-danger" /> Running Battles</h6>
                   </div>
                 </div>
-                {runningBattles.map((battle, index) => (
-                  <div className="col-12 card my-1 walletcard pt-2 px-0 mx-auto text-white" key={index}>
-                    <div className="row">
-                      <div className="col">
-                        <h6 className="mx-2 mb-0 d-flex">PLAYING FOR&nbsp;<span className="material-symbols-outlined text-success">payments</span> {battle.playingFor}</h6>
-                      </div>
-                      <div className="col d-flex justify-content-end me-2">
-                        <p className="mb-0">Prize&nbsp;</p>
-                        <h6 className="mb-0 d-flex"><span className="material-symbols-outlined text-success">payments</span> {battle.prize}</h6>
-                      </div>
-                    </div>
-                    <div className="card-body walletbody mt-2">
+                <div className="scroll-container">
+                  {runningBattles.map((battle, index) => (
+                    <div className="col-12 card my-1 walletcard pt-2 px-0 mx-auto text-white" key={index}>
                       <div className="row">
-                        <div className="col-4">
-                          <p className="text-light mb-0">{battle.players[0]}</p>
+                        <div className="col">
+                          <h6 className="mx-2 mb-0 d-flex">PLAYING FOR&nbsp;<span className="material-symbols-outlined text-success">payments</span> {battle.playingFor}</h6>
                         </div>
-                        <div className="col-4 d-flex justify-content-center">
-                          <img src="./images/versus.png" className="rounded-circle mx-1" style={{ width: '25%', height: 25 }} alt="" />
+                        <div className="col d-flex justify-content-end me-2">
+                          <p className="mb-0">Prize&nbsp;</p>
+                          <h6 className="mb-0 d-flex"><span className="material-symbols-outlined text-success">payments</span> {battle.prize}</h6>
                         </div>
-                        <div className="col-4 d-flex justify-content-end">
-                          <p className="text-light text-end mb-0">{battle.players[1]}</p>
+                      </div>
+                      <div className="card-body walletbody mt-2">
+                        <div className="row">
+                          <div className="col-4">
+                            <p className="text-light mb-0">{battle.players[0]}</p>
+                          </div>
+                          <div className="col-4 d-flex justify-content-center">
+                            <img src="./images/versus.png" className="rounded-circle mx-1" style={{ width: '25%', height: 25 }} alt="" />
+                          </div>
+                          <div className="col-4 d-flex justify-content-end">
+                            <p className="text-light text-end mb-0">{battle.players[1]}</p>
+                          </div>
                         </div>
                       </div>
                     </div>
-                  </div>
-                ))}
+                  ))}
+                </div>
+             
 
                 
 
@@ -163,23 +186,30 @@ function SecondPage() {
                 
                   <div className="col-12 card my-1 walletcard pt-2 px-0 mx-auto text-white" >
                     <div className="row">
-                      <div className="col">
-                        <h6 className="mx-2 mb-0 d-flex">PLAYING FOR </h6>
-                      </div>
+                      
                       
                     </div>
                     <div className="card-body walletbody mt-2">
                       <div className="row">
                       <div className="col-12 my-2">
-                        <input
+                        <input className='create-battles-input'
                           type="text"
-                          placeholder="Enter Your Name"
-                          // onChange={(e) => setName(e.target.value)}
-                          // disabled={isOtpFieldsShown} // Disable the input field if OTP fields are shown
+                          onChange={(e)=>setCreateBattles(e.target.value)}
+                          placeholder='Create Battles'
+                          
                         />
+                      
                       </div>
+                     
                       </div>
+                    <button
+                      className="bg-orange-create-battles-button btn "
+
+                    >
+                 Submit
+                    </button>
                     </div>
+                    
                   </div>
                 
 
